bb925278dc1700e19630f86163038aa46365fb6e
Shantanu Saxena <shantanu.saxena1997@gmail.com>
2026-04-24 17:21:20 +0530

    Use env variable for API base URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:4000/api";
+const API_BASE = import.meta.env.VITE_API_URL;
 
 async function request(path, options = {}) {
   const res = await fetch(`${API_BASE}${path}`, options);
